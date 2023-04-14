75acb0929ab20e245481440b97f4045d375a62e4
priscillia <priscillia.g.pro@outlook.fr>
2023-04-14 14:35:34 +0200

    MAJ

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,10 +1,10 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { pokemonInterface } from "../entities/pokemon-interface";
-import ThumbnailCard from "../components/components/Thumbnail";
 import LoaderPokeball from "../components/components/Loader-pokeball";
+import ThumbnailCard from "../components/components/Thumbnail";
 
-const Pokedex = () => {
+function Pokedex() {
   const [data, setData] = useState<pokemonInterface[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -24,55 +24,58 @@ const Pokedex = () => {
   }, []);
 
   const chunkSize = 18;
-  const [startIndex, setStartIndex] = useState(0);
+  const [startIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(chunkSize);
 
   const handleClickLoadMore = () => {
-    setStartIndex(startIndex + chunkSize);
     setEndIndex(endIndex + chunkSize);
   };
+
   const handleClickLoadLess = () => {
-    setStartIndex(startIndex - chunkSize);
     setEndIndex(endIndex - chunkSize);
   };
-  return isLoading === true ? (
-    <LoaderPokeball />
-  ) : (
-    <div className="relative flex gap-4 overflow-hidden flex-wrap py-12 mb-16  place-content-center">
-      {data.slice(startIndex, endIndex).map((pokemon: pokemonInterface) => {
-        const id = pokemon.id;
-        return (
-          <ThumbnailCard
-            link={`/pokemon/${id}`}
-            key={id}
-            imageUrl={pokemon.image}
-            imageUrlBis={pokemon.image}
-            imageUrlTitleAlt={pokemon.name}
-            imageUrlTitle={pokemon.name}
-          />
-        );
-      })}
 
-      <div className="flex justify-center w-full my-4">
-        {data.length > endIndex && (
-          <button
-            onClick={handleClickLoadLess}
-            className="text-base font-semibold rounded-lg px-4 py-2 bg-white text-gray-700 hover:bg-gray-200"
-          >
-            Voir moins
-          </button>
-        )}
-        {endIndex < data.length && (
-          <button
-            onClick={handleClickLoadMore}
-            className="text-base font-semibold rounded-lg px-4 py-2 bg-white text-gray-700 hover:bg-gray-200"
-          >
-            Voir plus
-          </button>
-        )}
-      </div>
+  return (
+    <div className="relative flex gap-4 overflow-hidden flex-wrap py-12 mb-16  place-content-center">
+      {isLoading ? (
+        <LoaderPokeball />
+      ) : (
+        <>
+          {data.slice(startIndex, endIndex).map((pokemon: pokemonInterface) => {
+            const id = pokemon.id;
+            return (
+              <ThumbnailCard
+                link={`/pokemon/${id}`}
+                key={id}
+                imageUrl={pokemon.image}
+                imageUrlBis={pokemon.image}
+                imageUrlTitleAlt={pokemon.name}
+                imageUrlTitle={pokemon.name}
+              />
+            );
+          })}
+          <div className="flex justify-center w-full my-4">
+            {startIndex > 0 && (
+              <button
+                onClick={handleClickLoadLess}
+                className="text-base font-semibold rounded-lg px-4 py-2 bg-white text-gray-700 hover:bg-gray-200"
+              >
+                Afficher moins
+              </button>
+            )}
+            {endIndex < data.length && (
+              <button
+                onClick={handleClickLoadMore}
+                className="text-base font-semibold rounded-lg px-4 py-2 bg-white text-gray-700 hover:bg-gray-200"
+              >
+                Afficher plus
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
-};
+}
 
 export default Pokedex;
